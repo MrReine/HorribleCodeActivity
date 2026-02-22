f9beb26b2bc9d9cfe67d37c399fc0b09b4994569
Alex Reine <alexreine712@gmail.com>
2026-02-22 14:33:08 -0500

    feat: added main function in goodcode.py

--- goodcode.py
+++ goodcode.py
@@ -15,3 +15,29 @@ def get_nums():
         except ValueError:
             print("Please enter a numbers.")
     return num1, num2
+
+def main():
+    operations = {
+        "1": (add, "sum"),
+        "2": (sub, "difference"),
+        "3": (mult, "product"),
+        "4": (div, "quotient"),
+    }
+
+    while True:
+        print("\n--- Calculator ---")
+        print("1: Add\n2: Sub\n3: Mult\n4: Div")
+        choice = input("Enter your choice: ")
+
+        if choice in operations:
+            num1, num2 = get_nums()
+            calc_func, op_name = operations[choice]
+
+            result = calc_func(num1, num2)
+            print(f"The {op_name} of {num1} and {num2} is {result} ")
+            break
+        else:
+            print("Please enter a valid choice.")
+
+if __name__ == "__main__":
+    main()
