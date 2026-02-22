d02ab8ba96075dbe6e902707d27f747c25504935
Alex Reine <alexreine712@gmail.com>
2026-02-22 14:01:26 -0500

    feat: added div function in badcode.py

--- badcode.py
+++ badcode.py
@@ -15,3 +15,10 @@ def mul():
     num2 = int(input("Enter second number: "))
     res = num1 * num2
     print(f"The product is {res}")
+
+def div():
+    num1 = int(input("Enter first number: "))
+    num2 = int(input("Enter second number: "))
+    res = num1 / num2
+    print(f"The division is {res}")
+
